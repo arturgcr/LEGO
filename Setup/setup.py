02e5a651e0c9ca85489784733eb9effae2965f8c
RebeccaGS <Rebecca.gomsim@gmail.com>
2022-02-05 23:17:08 -0300

    setup introdução

--- Setup/setup.py
+++ Setup/setup.py
@@ -15,110 +15,21 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
-class SensorLEGO():
-
-    def __init__(self, tipo, porta, lado, tamanhodofiltro=1):
+class Setup():
+    
+    def __init__(self, motores):
         
-        if porta==1:
-            porta=port.S1
-        elif porta==2:
-            porta=port.S2
-        elif porta==3:
-            porta=port.S3
-        elif porta==4:
-            porta=port.S4
         
-        if tipo == 'ultrassom' or 'ultrasson':
-                self.sensor = UltrasonicSensor(porta)
-        if tipo == 'infravermelho' or 'infrared':
-                self.sensor = InfraredSensor(porta)    
+    def arcoInicial(self,tempo):
+    
+    def manobraInicial(self,tempo):
 
-    self.lado = lado
-    self.tamanhoDoFiltro = tamanhodofiltro
-    self.listaFiltro = [0] * tamanhodofiltro
-    self.index=0
+    def armadilhaInicial(self,tempo):
 
-     # func que verefica se resultado eh vdd ou falso (sensores naturalmente tem um acumulo
-     #  de erros com o tempo, resultando em falsos positivos e falsos negativos)
-    def filtro(self):
-        
-        medicao = self.sensor.distance()
-        self.listaFiltro[self.index] = medicao
-        
-        if self.index < self.tamanhodofiltro:
-            self.index +=1
-        else:
-            self.index=0
-            
-        for i in self.listaFiltro:
-            if i != medicao:
-                return False     #funcao retorna true caso o filtro "aprove" o reusltado da medicao e false caso contrario
-                
-        return True
-            
-    # se filtro aprovado, confia no resultado e entra em def enxergando; enxergando afirma se ta vendo oponente ou nao    
-    def enxergando (self,limiar):
-        if self.sensor.distance() < limiar:
-            return True
-        else:
-            return False
-            
-            
-    # em sensoriamento: se filtro aprovado, entrar em def enxergando;
+    def selecionarEstrategia(self):
 
-class Sensoriamento():
-    def __init__(self, listadesensores, kp, kd, ki=0, limiar=40):
-    #lembrando q esse sensordireita, sensoresquerda e sensormeio são OBJETOS herdados da classe sensor  
-        self.sensoresDireita=[]
-        self.sensoresEsquerda=[]
+    def loopBotao(self): #string ? >diagrama de classes<
 
-        for i in listadesensores:
-            if i.lado == self.esquerda:
-                self.sensoresEsquerda.append(i) #adiciona i à lista da esquerda
-            if i.lado == self.direita:
-                self.sensoresDireita.append(i) #adiciona i à lista da direita
-        
-        self.kp=kp
-        self.kd=kd
-        self.ki=ki
-
-        self.listadesensores=listadesensores
-
-        self.limiar = limiar   #não vem como argumento da init, então pode entrar direto na classe. no diagram de classes está dizendo q pe default....
-                                   #um possível problema para essa abordagem é o fato de q o infravermelho não vem em cm, e o nxt med em cm, não mm
-                                   #converter os dados do infravermelho é algo q não implementei em geral
-
-    def verificalado(self):
-        x=0
-        y=0
-        for i in self.sensoresDireita: #sensoresdireita e sensoresesquerda seriam as listas com os sensores de cada lado
-            if i.enxergando(tem que indentificar o limiar)==True:
-                x+=1
-                y=1
-        for i in self.sensoresEsquerda:                             #é importante notar, q da forma como eu optei por montar o filtro aqui haveria uma defasagem de
-            if i.enxergando(tem que indentificar o limiar)==True:   #tempo entre as medições, dependendo do tamanho do filtro. pra evitar isto bastria implementar
-                x-=1                                                #o filtro no resultado dafunção verifica lado, ao ives de no final da função enxergando, isso é facil de mudar
-                x=1
-        
-        if x>0:
-            return "direita"    #nessa altura, o código ja sabe onde o adversario está.
-        if x<0:
-            return "esquerda"
-        if x=0 and y=1:
-            return "frente"
-        if y=0:
-            return "atrás"
-        
-        x=0
-        y=0
-
-        #Essa parte nã foi definida
-        #inclusive talvez seja melhor repensar, ao invez de retornar o erro talvez ela cria um atributo erro? não sei...
-
-    def verificaPerto(self): #funcao com limiar menor p garantir o full pa frente, ainda não implementei e não tem no diagrama de classes
-
-    def PID(self, erro, erroAnterior):
-        return self.kp * erro + self.kd * (erro- erroAnterior)
 
 # Write your program here.
 ev3.speaker.beep()    
