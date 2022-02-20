2b3d7892905bd8896c6cb8fd4c1d0ef8d44a6ce2
RebeccaGS <Rebecca.gomsim@gmail.com>
2022-02-20 10:18:11 -0300

    Inicio setup

--- Setup/setup.py
+++ Setup/setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+from re import X
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -7,28 +8,47 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO    if porta==1:
-        
 # Click "Open user guide" on the EV3 extension tab for more information
-
 # Create your objects here.
 ev3 = EV3Brick()
 
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Objetivo: Classe que ajusta robo antes dos segundos iniciais da luta - definindo direcao inicial e estrategia atraves do pressionamento de botoes
 class Setup():
     
     def __init__(self, motores):
-        
-        
+        self.motores = motores 
+
+    # descobrir botao direcao e estrategia e executar estrategia
+    def selecionarEstrategia(self): 
+        while true:
+            if buttons.pressed()[0] = X
+                self.estrategia = (arcoInicial ou outro)
+            
+        wait 1000
+
+        while true:
+            if buttons.pressed()[0] = X
+                self.direcao = (direita ou outro)
+
+    def executaEstrategia(self):
+        if self.estrategia = arcoInicial:
+            # chama arcoInicial por exemplo
+
     def arcoInicial(self,tempo):
-    
+        if self.direcao = esquerda
+        #infos oq motores fazem
     def manobraInicial(self,tempo):
 
     def armadilhaInicial(self,tempo):
 
-    def selecionarEstrategia(self):
-
-    def loopBotao(self): #string ? >diagrama de classes<
+    
 
 
 # Write your program here.
