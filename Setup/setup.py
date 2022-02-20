47213b70c59f1a2a71aad6e70f1b5bcf7827841f
RebeccaGS <Rebecca.gomsim@gmail.com>
2022-02-20 12:20:22 -0300

    Setup botoes + chamar funcs implementados
    Falta: configurar motores para ações iniciais

--- Setup/setup.py
+++ Setup/setup.py
@@ -8,44 +8,65 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# This program requires LEGO    if porta==1:
-# Click "Open user guide" on the EV3 extension tab for more information
-# Create your objects here.
 ev3 = EV3Brick()
 
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 # Objetivo: Classe que ajusta robo antes dos segundos iniciais da luta - definindo direcao inicial e estrategia atraves do pressionamento de botoes
+import time
 class Setup():
     
     def __init__(self, motores):
-        self.motores = motores 
+        self.motores = motores
+        self.pressionado = 0 #variavel para encerrar loop / se 0, botao nao pressionado, se 1, botao pressionado
+    
 
-    # descobrir botao direcao e estrategia e executar estrategia
+    # descobrir botao direcao e estrategia
     def selecionarEstrategia(self): 
-        while true:
-            if buttons.pressed()[0] = X
-                self.estrategia = (arcoInicial ou outro)
+        while (pressionado == 0):
+            if Button.RIGHT in buttons.pressed(): #duvida: buttons.pressed() ou brick.buttons.pressed()
+                pressionado = 1
+                self.estrategia = 1 # arcoInicial
+            elif Button.LEFT in buttons.pressed():
+                pressionado = 1
+                self.estrategia = 0 # manobraInicial
+            elif Button.CENTER in buttons.pressed():
+                pressionado = 1
+                self.estrategia = -1 # armadilhaInicial
+
+        time.sleep(1) # espera por 1 segundo
+        pressionado = 0
+
+        while (pressionado == 0):
+            if Button.UP in buttons.pressed():
+                pressionado = 1
+                self.direcao = 1 # direita
+            if Button.DOWM in buttons.pressed():
+                pressionado = 1
+                self.direcao = -1 # esquerda
             
-        wait 1000
 
-        while true:
-            if buttons.pressed()[0] = X
-                self.direcao = (direita ou outro)
-
+    # chama as respectivas acoes selecionadas
     def executaEstrategia(self):
-        if self.estrategia = arcoInicial:
-            # chama arcoInicial por exemplo
+        if self.estrategia == 1:
+            self.arcoInicial(self,tempo)
+        elif self.estrategia == 0:
+            self.manobraInicial(self,tempo)
+        elif self.estrategia == -1:
+            self.armadilhaInicial(self,tempo)
+
 
     def arcoInicial(self,tempo):
-        if self.direcao = esquerda
+        if self.direcao = 1 #direita
         #infos oq motores fazem
+        elif self.direcao = -1 #esquerda
+   
+   
     def manobraInicial(self,tempo):
 
+  
     def armadilhaInicial(self,tempo):
 
     
