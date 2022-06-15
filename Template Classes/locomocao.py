cc8684038f4db37b7783f594687163f5cd879b3e
Abner Freitas <defreitasabner@gmail.com>
2022-06-15 10:29:53 -0300

    Aplicando pequenas refatorações para padronizar o código.

--- Template Classes/locomocao.py
+++ Template Classes/locomocao.py
@@ -16,7 +16,7 @@ ev3 = EV3Brick()
 
 class Locomocao():
       
-    def __init__(self, strMotorDireita, strMotorEsquerdo, invertido = 'DEFAULT'):
+    def __init__(self, strMotorDireita, strMotorEsquerdo, invertido='DEFAULT'):
         # comecei definindo variaveis e pegando motores e respectivas portas (caso venham 2 ou mais motores)
         # ao inves de string, definir como array pois motores ja vem separados: [a, b, c, d]
         # caso nao possa, mudar modo com o qual a função define os motores (acredito que pode e eh mais eficiente)
@@ -43,33 +43,33 @@ class Locomocao():
             self.sentido_esquerda = -1
             
     # Aplica o valor de pwm [-100, 100] nas rodas do lado esquerdo.
-    def AplicarRodaEsquerda ( self, pwm):
+    def AplicarRodaEsquerda(self, pwm):
         for motor in self.motores_esquerda :
-            motor.dc( -pwm * self.sentido_esquerda)
+            motor.dc(-pwm * self.sentido_esquerda)
 
     # Aplica o valor de pwm [-100, 100] nas rodas do lado direito.
-    def AplicarRodaDireita ( self, pwm):
+    def AplicarRodaDireita(self, pwm):
         for motor in self.motores_direita :
-            motor.dc( -pwm * self.sentido_direita)
+            motor.dc(-pwm * self.sentido_direita)
 
     # usando as funcs acima para ir pra frente com a mesma potencia
-    def reta( self, pwm = 100 ):
-        self.AplicarRodaEsquerda( -pwm )
-        self.AplicarRodaDireita ( -pwm )
+    def reta(self, pwm=100):
+        self.AplicarRodaEsquerda(-pwm)
+        self.AplicarRodaDireita (-pwm)
 
     # talvez arco nao esteja rodando pras duas direcoes, e sim, so pra direita (se sim, criar um if e inverter sinais)
-    def arco (self, velocidadeLinear = 100, velocidadeAngular=15): # [Vang: que é metade da diferença de potencia entre os motores]
-        self.AplicarRodaEsquerda ( velocidadeLinear + velocidadeAngular )
-        self.AplicarRodaDireita ( velocidadeLinear - velocidadeAngular )
+    def arco (self, velocidadeLinear=100, velocidadeAngular=15): # [Vang: que é metade da diferença de potencia entre os motores]
+        self.AplicarRodaEsquerda (velocidadeLinear + velocidadeAngular)
+        self.AplicarRodaDireita (velocidadeLinear - velocidadeAngular)
 
     #  se gira em torno do proprio eixo para direita, motor esq vai pra trás (-1) na potencia pwm, e
     #  motor dir vai pra frente na potencia pwm, o inverso eh valido, ficando:
-    def giro(self, pwm = 100):
-        self.AplicarRodaEsquerda( pwm )
-        self.AplicarRodaDireita ( -pwm )
+    def giro(self, pwm=100):
+        self.AplicarRodaEsquerda(pwm)
+        self.AplicarRodaDireita (-pwm)
  
     # Pára o motor usando fricção e a tensão que este gira na inércia, atua como um freio fraco.
-    def Frear (self):
+    def Frear(self):
         for motor in self.motores_direita :
             motor.brake()
         for motor in self.motores_esquerda :
