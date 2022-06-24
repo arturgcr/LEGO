b9ff2d71f6112ba9b298480d433d0188fbee9ce4
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-24 13:05:29 -0300

    Refatoradas as class PID, SensorLEGO e Sensoriamento.

--- Template Classes/sensoriamento.py
+++ Template Classes/sensoriamento.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+ev3 = EV3Brick()
+
 class Sensoriamento():
     
     def __init__(self, lista_sensores, visto_ultimo, limiar=40):
