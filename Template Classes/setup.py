9395d4ae922b6c0d3e0609223d72fc6cf5ed15e1
Sérgio Medeiros <sergiomedeiros@poli.ufrj.br>
2022-06-25 14:53:04 -0300

    Alterações na Setup
    Finalização da primeira etapa da seleção de estratégia do setup

--- Template Classes/setup.py
+++ Template Classes/setup.py
@@ -26,61 +26,78 @@ ev3 = EV3Brick()
 # Objetivo: Classe que ajusta robo antes dos segundos iniciais da luta - definindo direcao inicial e estrategia atraves do pressionamento de botoes
 import time
 import locomocao
+
+
 class Setup():
     
     def __init__(self, motores): # # Alterar tempos correspondentes ao robo
+
         self.motores = motores  # Motores: objeto vindo de Locomoção
-        self.pressionado = 0    # variavel para encerrar loop / se 0, botao nao pressionado, se 1, botao pressionado
-        self.botao_direita = Button.RIGHT 
-        self.botao_esquerda = Button.LEFT
+        
+        # Botões do Brick considerando que o Brick tá de lado e com tela para direita do piloto
+        self.botao_direita = Button.UP # Botão para direita é o botão de cima com o Brick de lado
+        self.botao_esquerda = Button.DOWN
         self.botao_centro = Button.CENTER
-        self.botao_cima = Button.UP
-        self.botao_baixo = Button.DOWN
+        self.botao_cima = Button.LEFT
+        self.botao_baixo = Button.RIGHT
+
+        self.pressionado = 0    # variavel para encerrar loop / se 0, botao nao pressionado, se 1, botao pressionado
+        self.tipo_estrategia = 'padrao' # Variável para definir se será estratégia padrão ou de desempate
+
 
     def selecionarCorrecao(self):
-        while (pressionado == 0):
-            if self.botao_cima in ev3.buttons.pressed():
-                pressionado = 1
+        
+        while (self.pressionado == 0):
+            if self.botao_direita in ev3.buttons.pressed(): #Faz uma correção rotacionando 90º sobre o próprio eixo para a direita
+                self.pressionado = 1
                 self.direcao = 1  # direita
-            if self.botao_baixo in ev3.buttons.pressed():
-                pressionado = 1
+            elif self.botao_centro in ev3.buttons.pressed():
+                self.pressionado = 1
+            elif self.botao_esquerda in ev3.buttons.pressed():
+                self.pressionado = 1
                 self.direcao = -1 # esquerda
-
+            elif self.botao_cima in ev3.buttons.pressed():
+                self.pressionado = 1
+                self.tipo_estrategia = 'desempate'        
+        
+        time.sleep(1)
+        self.pressionado = 0
+                
                 
     # descobrir botao direcao e estrategia
     def selecionarEstrategia(self): 
-        while (pressionado == 0):
+        while (self.pressionado == 0 and self.tipo_estrategia == 'padrao'):
             if self.botao_direita in ev3.buttons.pressed(): #duvida: buttons.pressed() ou brick.buttons.pressed()
-                pressionado = 1
+                self.pressionado = 1
                 self.estrategia = 'arcoInicial' 
             elif self.botao_esquerda in ev3.buttons.pressed():
-                pressionado = 1
+                self.pressionado = 1
                 self.estrategia = 'manobraInicial' 
             elif self.botao_centro in ev3.buttons.pressed():
-                pressionado = 1
+                self.pressionado = 1
                 self.estrategia = 'armadilhaInicial'
             #elif self.botao_cima
             #elif self.botao_baixo
 
         time.sleep(1) # espera por 1 segundo
-        pressionado = 0
+        self.pressionado = 0
 
     def selecionarDirecaoMovimento(self):
-        while (pressionado == 0):
+        while (self.pressionado == 0):
             if self.botao_cima in ev3.buttons.pressed():
-                pressionado = 1
+                self.pressionado = 1
                 self.direcao = 1  # direita
             if self.botao_baixo in ev3.buttons.pressed():
-                pressionado = 1
+                self.pressionado = 1
                 self.direcao = -1 # esquerda
     
     def selecionarDirecaoSensoriamento(self):
-        while (pressionado == 0):
+        while (self.pressionado == 0):
             if self.botao_cima in ev3.buttons.pressed():
-                pressionado = 1
+                self.pressionado = 1
                 self.direcao = 1  # direita
             if self.botao_baixo in ev3.buttons.presses():
-                pressionado = 1
+                self.pressionado = 1
                 self.direcao = -1 #esquerda 
 
 
