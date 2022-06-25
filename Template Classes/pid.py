f986d45749becc62e045f5eb395144c7f5e9230f
Abner Freitas <defreitasabner@gmail.com>
2022-06-25 19:26:44 -0300

    Transferido o cálculo do erro para o sensoriamento, que será passado para o pid fazer os últimos cálculos.

--- Template Classes/pid.py
+++ Template Classes/pid.py
@@ -11,37 +11,22 @@ ev3 = EV3Brick()
 
 class PID:
     
-    def __init__(self, kp, kd, ki, sensoriamento):
+    def __init__(self, kp, kd, ki, erro):
         
         self.kp = kp
         self.kd = kd
         self.ki = ki
 
-        self.erro = 0
-        self.erro_passado = 0
-
-        self.sensoriamento = sensoriamento
-
-
-    def calcula_erro(self):  # o erro é dado pela diferença entre a medição dos sensores
-        for sensor in self.sensoriamento.sensores_direita:
-            somaDireita += sensor.distance()
-        mediaDireita = somaDireita/len(self.sensoresDireita)
-
-        for sensor in self.sensoriamento.sensoresEsquerda:
-            somaEsquerda += sensor.distance()
-        mediaEsquerda = somaEsquerda/len(self.sensoresEsquerda)
-
-        self.erro = abs(mediaDireita - mediaEsquerda)/100  # está em cm
-        return self.erro
+        self.erro = erro
+        self.erro_anterior = 0
 
     # junção entre PID e verificaPerto
     def calcula_pid(self):
         if self.erro < 5:
             self.erro = 0
         else:
-            self.erro = self.erro - self.erroAnterior        #Adicionado essa linha para inserir novo valor de erro conforme código em blocos
+            self.erro = self.erro - self.erro_anterior        #Adicionado essa linha para inserir novo valor de erro conforme código em blocos
             PID = self.kp * self.erro + self.kd * self.erro 
-            self.erroAnterior = self.erro
+            self.erro_anterior = self.erro
         print(PID)
         return PID
--- Template Classes/sensoriamento.py
+++ Template Classes/sensoriamento.py
@@ -27,20 +27,27 @@ class Sensoriamento():
         # um possível problema para essa abordagem é o fato de q o infravermelho não vem em cm, e o nxt med em cm, não mm
         # converter os dados do infravermelho é algo q não implementei em geral
         self.visto_ultimo = visto_ultimo
+        # recebe o erro na leitura dos sensores
+        self.erro = 0
 
     def busca_oponente(self):
         nao_viu_nada = True     # Varíavel que determina se não vimos o robô inimigo / booleano
         enxergando_inimigo = 0  # Varíavel feita para definir de que lado o robô foi visto positivo direita e negativo esquerda
-        
+        # Variável que guarda a distânica vista por cada sensor
+        distancia_sensor_esquerda = 0
+        distancia_sensor_direita = 0
+
         for sensor in self.sensores_direita:  # sensoresdireita e sensoresesquerda seriam as listas com os sensores de cada lado
             if sensor.enxergando(self.limiar):   # tem que indentificar o limiar
                 enxergando_inimigo += 1
                 nao_viu_nada = False
+                distancia_sensor_direita = sensor.ultima_distancia_autorizada
 
         for sensor in self.sensores_esquerda:
             if sensor.enxergando(self.limiar):
                 enxergando_inimigo -= 1
                 nao_viu_nada = False
+                distancia_sensor_esquerda = sensor.ultima_distancia_autorizada
 
         # Se nenhum dos sensores viu, então retorna a direção de visto por último
         if nao_viu_nada:
@@ -48,5 +55,10 @@ class Sensoriamento():
                 return self.visto_ultimo
         else:
             self.visto_ultimo = enxergando_inimigo
+            self.erro = self.calcula_erro(distancia_sensor_esquerda, distancia_sensor_direita)
             return enxergando_inimigo
+
+    def calcula_erro(self, distancia_sensor_esquerda, distancia_sensor_direita):  # o erro é dado pela diferença entre a medição dos sensores
+        erro = abs(distancia_sensor_esquerda - distancia_sensor_direita)/100  # está em cm
+        return erro
 
