9535429f7f1c5a97090b318926df6900dac7e686
Sérgio Medeiros <sergiomedeiros@poli.ufrj.br>
2022-06-25 20:03:32 -0300

    Renomeação para português e finalização da estrategia Junto com o renomeamos o arquivo e a classe setup para inicialização afim de manter o código em português. Além disso, finalizamos a programação das seleções. Construímos novas atributos, um para o ângulo da correção e outro para a direção do sensor. Já para interromper o loop retiramos o possiçao==1 no final do elif e colocamos um break.  Não mexemos na execução de estratégia Co-authored-by: MarcelJavierre <MarcelJavierre@users.noreply.github.com>

---
+++ Template Classes/inicializacao.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env pybricks-micropython
+
+'''
+Modulo responsavel pelo ajuste do robo antes dos segundos iniciais da
+luta. Aqui sao definidos a direcao inicial e estrategia atraves do
+pressionamento de botoes.
+'''
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Buttons, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+import locomocao
+
+# Definicao do objeto com os atributos e metodos relacionadas as peças LEGO
+ev3 = EV3Brick()
+
+# CONFIGURAÇÕES INICIAIS - NÃO ESQUECER ---------------
+# -> Aqui colocar os valores a serem alterados e separar por cada robo (ex: tempoManobra - treta: 5) 
+# -> Treta - Codigo antigo - TESTAR A PARTIR DE:
+#    erroBase = 15
+#    kp = 2
+#    ki = 0
+#    kd = 1.2
+
+class Inicializacao():
+    '''
+    Classe com os metodos e atributos responsaveis pelo ajuste do robo
+    nos segundos iniciais da luta. Aqui sao definidos a direcao inicial
+    e estrategia atraves do pressionamento de botoes.
+    '''
+    def __init__(self, motores): # Alterar tempos correspondentes ao robo
+        '''
+        Metodo construtor. Define os atributos utilizados nas selecoes.
+
+        Self@Inicializacao, ? -> None
+        '''
+        # Motores: objeto vindo de Locomoção
+        self.motores = motores
+        
+        # Botoes do Brick considerando que o Brick tá de lado e com tela para direita do piloto
+        self.botao_central   = Buttons.CENTER
+        self.botao_direito  = Buttons.UP    # Botão para direita é o botão de cima com o Brick de lado
+        self.botao_esquerdo = Buttons.DOWN  # Botao para esquerda é o botao de baixo  do brick de Lado 
+        self.botao_cima     = Buttons.LEFT  # Botao para cima é o botao da esquerda do brick virado de lado 
+        self.botao_baixo    = Buttons.RIGHT # Botao para baixo é o botao da esquerda do brick virado para o lado direito
+
+        # Atributo que define o angulo que o robo gira na correcao
+        self.angulo_de_correcao = 0
+        
+        # Atributo que define a estrategia selecionada
+        self.estrategia_selecionada = 'padrao'
+
+        # Atributo que define o sentido do sensoriamento
+        self.direcao_do_sensoriamento = 'horario'
+
+    def selecionarCorrecao(self):
+        '''
+        Metodo com a primeira selecao que define o angulo da correcao ou
+        se o modo das estrategias e de desempate.
+        
+        Self@Inicializacao -> None
+        '''
+        # Na selecao da correcao, o brick ira acender uma luz na cor roxa
+        ev3.light(Color.PURPLE)
+        
+        # Enquanto algum botao nao for apertado
+        while True:
+            # Caso o botao direito tenha sido apertado, o angulo de correcao é de 90 graus para a direita
+            if self.botao_direito in ev3.buttons.pressed(): # Faz uma correção rotacionando 90 graus sobre o próprio eixo para a direita
+                self.angulo_de_correcao = 90
+                break
+            # Caso o botao esquerdo tenha sido apertado, o angulo de correcao é de 90 graus para a esquerda
+            elif self.botao_esquerdo in ev3.buttons.pressed():
+                self.angulo_de_correcao = -90
+                break
+            # Caso o botao do centro tenha sido apertado, o angulo de correcao é de 0 graus
+            elif self.botao_central in ev3.buttons.pressed():
+                self.angulo_de_correcao = 0
+                break
+            # Caso o botao de cima tenha sido apertado, vai para a rodada de desempate
+            elif self.botao_baixo in ev3.buttons.pressed():
+                self.estrategia_selecionada = 'desempate'
+                break
+        
+        time.sleep(1)
+
+    # descobrir botao direcao e estrategia
+    def selecionarEstrategia(self): # Segunda selecao 
+        """
+        Metodo com a segunda selecao que define a estretegia.
+
+        Self@Inicializacao -> None
+        """
+
+        # Verifica se o modo de estrategia eh padrao
+        if self.estrategia_selecionada == 'padrao':
+            # Na selecao das estrategias padroes, o brick ira acender uma luz na cor ciano
+            ev3.light(Color.CYAN)
+
+            # Enquanto algum botao nao for apertado
+            while True:
+                if self.botao_cima in ev3.buttons.pressed():
+                    self.estrategia_selecionada = 'armadilha_reta'
+                    break
+                elif self.botao_baixo in ev3.buttons.pressed(): 
+                    self.estrategia_selecionada = 'manobra_+_arco_re'
+                    break
+                elif self.botao_esquerdo in ev3.buttons.pressed():
+                    self.estrategia_selecionada  = 'manobra_+_arco'
+                    break
+                elif self.botao_direito in ev3.buttons.pressed():
+                    self.estrategia_selecionada  = 'arco'
+                    break
+                elif self.botao_central in ev3.buttons.pressed():
+                    self.estrategia_selecionada  = 'armadilha_+_arco'
+                    break
+
+        # Caso contrario, verifica se o modo de estrategia eh desempate
+        elif self.estrategia_selecionada == 'desempate':
+            # Na selecao das estrategias de desempate, o brick ira acender uma luz na cor vermelha
+            ev3.light(Color.RED)
+
+            # Enquanto algum botao nao for apertado
+            while True:
+                if self.botao_central in ev3.buttons.pressed():
+                    self.estrategia_selecionada = 'giro_em_um_eixo'
+                    break
+            
+        # Espera por 1 segundo
+        time.sleep(1)
+
+    def selecionarDirecaoMovimento(self): # Selecao 3 movimentacao
+        """"
+        Metodo com a terceira selecao que escolhe a direcao da 
+        movimentacao do robo.
+        """ 
+        while (self.pressionado == 0):
+            if self.botao_direito in ev3.buttons.pressed(): 
+                self.direcao     = 1  # direita
+                break
+            elif self.botao_esquerdo in ev3.buttons.pressed():
+                self.direcao     = -1 # esquerda
+                break
+            elif self.botao_central in ev3.buttons.pressed():
+                self.estrategia_selecionada = 'Radar'
+                break
+            elif self.botao_baixo in ev3.buttons.pressed():
+                self.estrategia_selecionada = 'full re'
+                break
+            elif self.botao_cima in ev3.buttons.pressed():
+                self.estrategia_selecionada = 'full frente honesto'
+                break
+        
+        time.sleep(1)      
+    
+    def selecionarDirecaoSensoriamento(self):
+        """"
+        Metodo com a quarta e utima selecao que escolhe 
+        qual lado o robo vai comecar a sensoriar.
+        """"
+        while (self.pressionado == 0):
+            if self.botao_esquerdo in ev3.buttons.pressed():
+                self.direcao     = 1  # direita (sentido horario)
+                break 
+            if self.botao_direito in ev3.buttons.pressed():
+                self.direcao     = -1 #esquerda (Sentido anti horario) 
+                break
+
+    # chama as respectivas ações selecionadas
+    def executaEstrategia(self):
+        if self.estrategia == 'arcoInicial':
+            self.arcoInicial() # colocar valores Numericos padronizados de cada robo
+        elif self.estrategia == 'manobraInicial':
+            self.manobraInicial()
+        elif self.estrategia == 'armadilhaInicial':
+            self.armadilhaInicial()
+
+    def arcoInicial(self):             
+        locomocao.arco(Vlin, VAng*self.direcao) # Alterar Vlin e Vang correspondentes ao robo
+        time.sleep(tempo)                       # Alterar tempo
+        locomocao.giro(pwm*self.direcao)        # Alterar pwm correspondente ao robo - pra virar pro meio da arena novamente
+        time.sleep(tempo)                       # Alterar tempo
+
+    def manobraInicial(self):
+        locomocao.giro(-pwm*self.direcao)       # Alterar pwm correspondente ao robo
+        time.sleep(tempo)                       # Alterar tempo (geralmente 0.28) 
+        locomocao.arco(Vlin, VAng*self.direcao) # Alterar Vlin e Vang correspondentes ao robo
+        time.sleep(tempo)                       # alterar tempo
+        locomocao.giro(pwm*self.direcao)        # Alterar pwm correspondente ao robo - pra virar pro meio da arena novamente
+        time.sleep(tempo)                       # Alterar tempo 
+        
+    # descobrir se precisa ou nao da func giro em armadilhaInicial
+    def armadilhaInicial(self):
+        locomocao.arco(-Vlin, -VAng*self.direcao) # Alterar Vlin e Vang correspondentes ao robo
+        time.sleep(tempo)                         # alterar tempo
+        locomocao.giro(pwm*self.direcao)          # Alterar pwm correspondente ao robo - pra virar pro meio da arena novamente
+        time.sleep(tempo)                         # Alterar tempo
