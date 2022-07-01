3cfe5895ff352a3004e8e3ce8dfb157bb83ca345
Stalkerfish <silva.allison2002@gmail.com>
2022-07-01 17:43:09 -0300

    Adicionados novos ambientes de trabalho para o Treta e Violeta

---
+++ Treta RoboChallenge 22.1/estrategia.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env pybricks-micropython
+
+"""
+Módulo responsável pela definição da classe com os métodos e atributos
+relacionados às estratégias.
+"""
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+class Estrategia():
+    """
+    Classe com os métodos e atributos relacionados às estratégias. Esta
+    classe herda os métodos e atributos da classe Locomocao do módulo
+    locomocao.
+    """
+    def __init__(self, obj_locomocao):
+        """
+        Método construtor. Executa o método construtor da super classe
+        Locomocao.
+
+        Self@Estrategia, list[str], list[str], str -> None
+        """
+        self.motores = obj_locomocao
+    
+    def executa_correcao(self, angulo_correcao):
+        '''
+        Método que recebe um ângulo que, se for diferente de 0, corrige
+        o ângulo do robô 90° (para direita ou esquerda) antes de
+        executar a estratégia inicial de fato.
+
+        Self@Estrategia, int -> None
+        '''
+        # Verifica se o ângulo é diferente de 0
+        if angulo_correcao != 0:
+            # Verifica se o ângulo é igual a 90
+            if angulo_correcao == 35:
+                # Gira o robô no sentido horário
+                self.motores.giro(100)
+                wait(500)
+            # Verifica se o ângulo é igual a -90
+            elif angulo_correcao == -35:
+                # Gira o robô no sentido anti-horário
+                self.motores.giro(-100)
+                wait(500)
+        # Caso contrário, faz nada
+        else:
+            pass
+    
+    def executa_estrategia_inicial(self, estrategia_inicial_selecionada, direcao_estrategia_inicial):
+        """
+        Método que executa as respectivas ações selecionadas.
+        
+        Self@Estrategia, str, str -> None
+        """
+        if estrategia_inicial_selecionada == 'arco': #Pronto
+            self.arco(direcao_estrategia_inicial) # O método já define a direção (esquerda, direita)
+        elif estrategia_inicial_selecionada == 'comunismo': #Pronto
+            self.comunismo(direcao_estrategia_inicial)
+        elif estrategia_inicial_selecionada == 'capitalismo': #Pronto
+            self.capitalismo(direcao_estrategia_inicial)
+        elif estrategia_inicial_selecionada == 'moonwalk': #Pronto
+            self.moonwalk(direcao_estrategia_inicial)
+        elif estrategia_inicial_selecionada == 'full_frente_honesto': #Pronto
+            self.full_frente_honesto()
+        elif estrategia_inicial_selecionada == 'full_re_honesto': #Pronto
+            self.full_re_honesto()
+        else:
+            print('ATENCAO! Nenhuma estrategia selecionada')        
+
+
+    # Executa a estratégia de perseguição com base na leitura do sensores e do PID
+    def executa_estrategia_perseguicao(self, pid_convertido_pwm):
+        self.radar(pid_convertido_pwm)
+            
+    
+    # =================================== Estratégias Iniciais =========================================
+    def arco(self, direcao):
+        """Função que aciona o arco. Neste movimento, o robô deve ser posicionado de lado. Ao selecionar o lado,
+        o robô irá percorrer a borda da arena"""
+        velocidade_linear = 100
+        velocidade_angular = 50* -direcao
+        giro_mesmo_sentido = 100 * -direcao # valor para rotacionar na direção oposto que fez o arco
+        self.motores.arco(velocidade_linear, velocidade_angular) # Alterar Vlin e Vang correspondentes ao robo
+        print('iniciou o arco')
+        wait(2000) # o tempo pode variar para cada robô
+        self.motores.giro(giro_mesmo_sentido)
+        wait(550)
+        print('girou no msm sentido')
+        
+    
+
+    def bixo_piruleta(self, direcao, pwm):
+        """"O Robô começa de costas, na linha do adversário. O robo gira no eixo de apenas uma das rodas"""
+        self.motores.giro(pwm*direcao)
+        wait(1500)
+
+    # Manobra + Arco => segue reto por alguns segundos e executa um arco
+    def comunismo(self, direcao):
+        '''
+        Executa um curto movimento em linha reta e logo em seguida executa um arco e finaliza se voltando para o centro da arena.
+        '''
+        velocidade_linear = 100
+        velocidade_angular = 50 * -direcao
+        giro_sentido_oposto = 100 * -direcao
+        giro_mesmo_sentido = 100 * direcao
+        self.motores.reta() #frente
+        wait(200)
+        self.motores.giro(giro_mesmo_sentido) #angulo
+        wait(350)
+        self.motores.arco(velocidade_linear+20, velocidade_angular ) # Alterar Vlin e Vang correspondentes ao robo
+        wait(1200) # alterar tempo
+        self.motores.giro(giro_sentido_oposto) #angulo
+        wait(250)
+        
+   
+    # Arco de costas --> O robô posicionado de lado faz um arco para trás e depois um giro para o centro da arena
+    def moonwalk(self, direcao):
+        """Função que aciona o arco. Neste movimento, o robô deve ser posicionado de lado. Ao selecionar o lado,
+        o robô irá percorrer a borda da arena de costas"""  
+        print("Meu nome é Michael Jackson!")   
+        velocidade_linear = 200
+        velocidade_angular = 150 * direcao
+        giro_mesmo_sentido = 100 * -direcao # valor para girar o robô no mesmo sentido que a direção da estrategia 
+        self.motores.arco(-velocidade_linear, velocidade_angular) # Alterar Vlin e Vang correspondentes ao robo
+        print('velocidade linear:', velocidade_linear)
+        print('velocidade angular:', velocidade_angular)
+        wait(2500) # Alterar Tempo
+        self.motores.giro(giro_mesmo_sentido) # Alterar pwm correspondente ao robo - pra virar pro meio da arena novamente
+        wait(700)
+        
+    def full_frente_honesto(self):
+        '''Uma full frente honesta, nada mais nada menos. O robô apenas vai pra frente com tudo, cuidados devem ser tomados
+            com essa manobra '''
+        
+        velocidade = 100 
+        self.motores.reta(velocidade)
+        wait(5000)
+    
+
+    def full_re_honesto(self):
+        '''Uma full frente honesta, nada mais nada menos. O robô apenas vai pra frente com tudo, cuidados devem ser tomados
+            com essa manobra '''
+        velocidade = 100 
+        self.motores.re(velocidade)
+        wait(7000)
+
+    # Armadilha reta -
+    def baby_come_back(self, pwm):
+        """"O robô pode estar posicionado de qualquer forma, mas longe da borda da arena. Inicia dando uma ré, totalmente reta"""
+        pass
+
+    # Manobra + Arco invertida
+    def capitalismo(self, direcao):
+        """"O robô é posicionado de frente um pouco mais no centro na arena. Inicialmente vai para trás ( da ré). 
+        Faz o giro para uma direção selecionada (gira) e executa o arco na direção selecionada"""
+
+        velocidade_linear = 100
+        velocidade_angular = 50 * -direcao
+        giro_mesmo_sentido = 100 * direcao
+        giro_sentido_oposto = 100 * -direcao
+        #self.motores.reta(-velocidade_linear)
+        self.motores.re(velocidade_linear)
+        wait(450)
+        self.motores.giro(giro_mesmo_sentido)
+        wait(350)
+        self.motores.arco(velocidade_linear +20, velocidade_angular) #
+        wait(1700) # alterar tempo
+        self.motores.giro(giro_sentido_oposto) # Alterar pwm correspondente ao robo - pra virar pro meio da arena novamente
+        wait(500) # Alterar tempo
+
+    # ==================================================================================================
+    
+    # ============================ Estratégias de Perseguição ========================================== 
+    # gira ao redor de si até encontrar algo
+    def radar(self, pwm):
+        # Gira no sentido anti-horário com potência determinada pelo PID
+        if pwm != 0:
+            self.motores.giro(pwm) # direcao_oponente = -1
+        # Full frente com potência máxima, desconsiderando PID
+        if pwm == 0:
+            self.motores.reta(100)
+    # ==================================================================================================
+
+    """ # vai andando em arcos ate encontrar algo
+    # 100 e 15 eh valor arbitrario, teremos q testar e descobrir o real
+    def radarArco (self,erro):
+        if erro < 0:
+            self.motores.arco(100,-15)
+        if erro == 0:
+            self.motores.reta(100)
+        if erro > 0:
+            self.motores.arco(100,15) 
+
+    # vai andando em arcos ate encontrar algo
+    def radarPID (self, erro, vLin=0):
+        # na func arco - primeiro argumento: Vlin; segundo argumento: erro;
+        if erro != 0:
+            self.motores.arco(vLin,erro)
+        if erro == 0:
+            self.motores.reta(100) """
---
+++ Treta RoboChallenge 22.1/sensor.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import InfraredSensor, UltrasonicSensor # ultrassônico da violeta
+from pybricks.nxtdevices import UltrasonicSensor as nxtUltrasonicSensor # esse é o ultrassônico do treta
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class SensorDeOponente():
+    '''
+    Módulo de SensorLEGO
+    --------------------
+    Responsável por instanciar um sensor, definindo: tipo (str: `ultrassonico` ou `infravermelho`), porta (int: `1`, `2`, `3` ou `4`), a posição (str: `esquerda` ou `direita`) e o tamanho do filtro (`int`, por padrão recebe `None`)do sensor.
+    '''
+    def __init__(self, sensores, pesos, tipo, tamanho_filtro=1):
+        """
+        Self, dict[str, int], dict[str, int], str -> None
+        """
+        # Atributo para armazenar os sensores de oponente
+        self.sensores = sensores
+
+        # Atributo para armazenar os pesos dos sensores de oponente
+        self.pesoDosSensores = pesos
+
+        # Atributo para armazenar o tipo dos sensores de oponente
+        self.tipo = tipo
+
+        # Atributo para armazenar as leituras dos sensores
+        self.leituraDosSensores = {}
+        # Inicia todos os sensores como "False"
+        for sensor in sensores:
+            self.leituraDosSensores[sensor] = False
+
+        # Atributo que armazena se o oponente foi detectado ou não
+        self.oponenteDetectado = False
+
+        # Atributo que armazena o erro dos sensores
+        self.erro = 0
+
+        # Define as portas de cada sensor
+        for sensor in sensores:
+            if self.sensores[sensor] == 1:
+                self.sensores[sensor] = Port.S1
+            elif self.sensores[sensor] == 2:
+                self.sensores[sensor] = Port.S2
+            elif self.sensores[sensor] == 3:
+                self.sensores[sensor] = Port.S3
+            elif self.sensores[sensor] == 4:
+                self.sensores[sensor] = Port.S4
+
+        # Define o objeto de cada sensor
+        if self.tipo == "ultrassonico":
+            for sensor in sensores:
+                self.sensores[sensor] = UltrasonicSensor(self.sensores[sensor])
+        elif self.tipo == "nxtultrassonico":
+            for sensor in sensores:
+                self.sensores[sensor] = nxtUltrasonicSensor(self.sensores[sensor])
+        elif self.tipo == "infravermelho":
+            for sensor in sensores:
+                self.sensores[sensor] = InfraredSensor(self.sensores[sensor])
+
+        # guarda a última direção que foi detectado
+        self.visto_por_ultimo = -1
+        # guarda o valor que será usado para o tamanho do filtro durante a leitura dos sensores
+        self.tamanho_filtro = tamanho_filtro
+        # guarda o valor numérico da última medição autorizada pelos filtros
+        self.ultima_distancia_autorizada = 0 # essencial para organizar o cálculo do erro
+
+    def lerSensores(self, limiar = 400):
+        # Passa por todos os sensores e verifica se o oponente foi detectado ou não
+        for sensor in self.sensores:
+            filtro = [0] * self.tamanho_filtro
+            leitura = bool(self.sensores[sensor].distance() < limiar) 
+            if leitura:
+                for leitura_filtro in filtro:
+                    nova_leitura = bool(self.sensores[sensor].distance() < limiar)
+                    if nova_leitura == False:
+                        self.leituraDosSensores[sensor] = False
+                self.leituraDosSensores[sensor] = True
+            else:
+                self.leituraDosSensores[sensor] = False
+        # Atualiza o atributo que armazena se o oponente foi detectado ou não
+        self.oponenteDetectado = True in self.leituraDosSensores.values()
+
+        # Caso o oponente tenha sido detectado, calcula o erro dos sensores
+        if self.oponenteDetectado == True:
+            self.erro = self.calcularErro()
+
+    def calcularErro(self):
+        # Variável para armazenar a soma dos pesos dos sensores que detectaram o oponente
+        soma = 0
+
+        # Variável para armazenar o número de sensores que detectaram o oponente
+        numeroDeDeteccoes = 0
+
+        # Passa por todos os sensores
+        for sensor in self.sensores:
+            # Verifica se o sensor detectou o oponente
+            if self.leituraDosSensores[sensor] == True:
+                # Soma o peso do sensor na soma dos pesos dos sensores que detectaram o oponente
+                soma += self.pesoDosSensores[sensor]
+
+                # Incrementa 1 no número de detecções
+                numeroDeDeteccoes += 1
+        self.visto_por_ultimo = soma
+        # Retorna a média ponderada dos sensores que detectaram o oponente
+        return soma / numeroDeDeteccoes
+
+    # # Recebe o tamanho do filtro e cria uma lista de tamanho igual
+    # # Caso receba 0, não cria o filtro
+    # def criando_filtro(self, tamanho_filtro):
+    #     if tamanho_filtro != 0:
+    #         filtro = [0] * tamanho_filtro
+    #         return filtro
+    #     else:
+    #         return None
+    
+    # # Função que vai decidir como vai ser a medição do sensor, tornando a classe modular
+    # # Não está funcionando
+    # def medicao(self):
+    #     if isinstance(self.sensor, (UltrasonicSensor, InfraredSensor)):
+    #         distancia = self.sensor.distance()
+    #         return distancia
+    
+    # # funcão que verifica se o resultado eh vdd ou falso (sensores naturalmente tem um acumulo
+    # # de erros com o tempo, resultando em falsos positivos e falsos negativos)
+    # def filtrar(self):
+    #     for leitura_filtro in range(len(self.filtro)):
+    #         distancia = self.distancia()
+    #         if distancia == 0: # Converte o resultado para booleano, pq oq nos interessa é a presença apenas
+    #             distancia = False
+    #         elif distancia != 0:
+    #             distancia = True
+    #         self.filtro[leitura_filtro] = distancia
+    #         if self.filtro[leitura_filtro] != self.filtro[0]:
+    #             break
+    #     if False not in self.filtro:
+    #         return True
+    #     else:
+    #         return False
+    
+    # # se filtro aprovado, confia no resultado e entra em def enxergando; enxergando afirma se ta vendo oponente ou nao
+    # def enxergando(self, limiar):
+    #     distancia = self.sensor.distance()
+    #     if distancia < limiar:
+    #         if self.filtro != None:
+    #             if self.filtrar():
+    #                 self.ultima_distancia_autorizada = distancia
+    #                 return True
+    #             else:
+    #                 return False
+    #         return True
+    #     else:
+    #         return False
