688c978b5a0c5ee60f9118feb310f704ba6d5c94
julia.paoli <julia.paoli@poli.ufrj.br>
2022-07-01 18:47:55 -0300

    Descrição da função da rampa

--- Violeta RoboChallenge 22.1/locomocao.py
+++ Violeta RoboChallenge 22.1/locomocao.py
@@ -103,7 +103,7 @@ class Locomocao():
         Move o servo-motor responsável por liberar a rampa da violeta. Atualmente, está configurado para gerar 180° no sentido anti-horário (-180). Com a adição de mais servos-motores, esse método precisará ser revisto.
         '''
         for servo_motor in self.servo_motores:
-            servo_motor.run_angle(1000,-180) # acho que dessa forma, vai girar 180° no sentido anti-horário
+            servo_motor.run_angle(1000,-180) # função que faz o servo motor girar e cair a rampa. 1° parametro é de velocidade em deg/s e o 2° o angulo
         print('roda caiu')
 
     def mapy(self, valor_a_ser_convertido, minimo_da_entrada, maximo_da_entrada, minimo_da_saida,  maximo_da_saida):
